03c1f966254057d0f16cc673328eb7ab1f804655
Anthony Riley <anth1y@gmail.com>
2014-10-06 21:29:49 -0700

    testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+    test "product is not valid without a unique title" do
+     product = Product.new (title:    products(:ruby).title,
+                            description:  "yyy",
+                            price:     1,
+                            image_url:    "fred.gif")
+     assert product.invalid?
+     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+    end
+  end
